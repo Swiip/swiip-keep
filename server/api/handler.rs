43e4d29db6decfbafd485c4357a2e3810884d1ee
Matthieu Lux <matthieu.lux@protonmail.com>
2024-09-03 17:19:00 +0200

    Setup connection to vercel kv

--- server/api/handler.rs
+++ server/api/handler.rs
@@ -1,5 +1,12 @@
+use redis::{Client, Commands};
+use regex::Regex;
 use serde_json::json;
+use std::env;
 use vercel_runtime::{run, Body, Error, Request, Response, StatusCode};
+
+const REDIS_KEY: &str = "my_key";
+// TO CHANGE
+const REDIS_VALUE: &str = "this is the way!";
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -7,14 +14,27 @@ async fn main() -> Result<(), Error> {
 }
 
 pub async fn handler(_req: Request) -> Result<Response<Body>, Error> {
+    let kv_url = env::var("KV_URL")?;
+
+    println!("kv_url: {}", kv_url);
+
+    let url = Regex::new("^redis:")
+        .unwrap()
+        .replace_all(&kv_url, "rediss:")
+        .to_string();
+
+    println!("url: {}", url);
+
+    let client = Client::open(url)?;
+
+    let mut con = client.get_connection()?;
+
+    con.set(REDIS_KEY, REDIS_VALUE)?;
+
+    let message = format!("Hello, I wrote \"{}\" in Redis!", REDIS_VALUE);
+
     Ok(Response::builder()
         .status(StatusCode::OK)
         .header("Content-Type", "application/json")
-        .body(
-            json!({
-              "message": "Hello, world! now?"
-            })
-            .to_string()
-            .into(),
-        )?)
+        .body(json!({ "message": message }).to_string().into())?)
 }
